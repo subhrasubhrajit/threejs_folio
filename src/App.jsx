c61618c6f170519cbbc42827d8b587450b699853
subhrasubhrajit <nandysubhrajit12@gmail.com>
2024-10-29 22:30:49 +0530

    This is a new project based on THREE JS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Hero from "./sections/Hero"
+import Navbar from "./sections/Navbar"
+
+function App() {
+  return (
+    <main className="max-w-7xl mx-auto">
+      Hello 
+      <Navbar />
+      <Hero />
+    </main>
+  )
+}
+
+export default App
