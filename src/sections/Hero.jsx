a7264f7a35225af933a7de592c2c6c9680a1ada0
subhrasubhrajit <nandysubhrajit12@gmail.com>
2024-10-31 02:45:18 +0530

    todays update

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,19 @@
 import { PerspectiveCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import HackerRoom from "../components/HackerRoom";
+import { Suspense } from "react";
+import { calculateSizes } from '../constants/index';
+
+import CanvasLoader from "../components/CanvasLoader";
+import { useMediaQuery } from "react-responsive";
+
 const Hero = () => {
+  const isSmall = useMediaQuery({ maxWidth: 440 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
   return (
     <section className="min-h-screen w-full flex flex-col relative">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -10,8 +22,20 @@ const Hero = () => {
       </div>
       <div className="w-full h-full absolute inset-0">
         <Canvas className="w-full h-full">
-          <PerspectiveCamera makeDefault position={[0, 0, 30]} />
-          <HackerRoom />
+          <Suspense fallback={<CanvasLoader />}>
+            
+            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            
+            <HackerRoom 
+            scale={sizes.deskScale} 
+            position={sizes.deskPosition} 
+            rotation={[0, -Math.PI, 0]} />
+          
+          <ambientLight intensity={1} />
+          <directionalLight  position={[10, 10, 10]} intensity={0.5}/>
+          
+          </Suspense>
+          
         </Canvas>
       </div>
     </section>
