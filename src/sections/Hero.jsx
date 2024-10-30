6ee0e759167df74042ae955ac8aa9760de0a1aa0
subhrasubhrajit <nandysubhrajit12@gmail.com>
2024-10-30 23:31:57 +0530

    new addition

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
-
+import { PerspectiveCamera } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import HackerRoom from "../components/HackerRoom";
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="min-h-screen w-full flex flex-col relative">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Hi, I am Subhrajit <span className="waving-hand">👋</span></p>
+        <p className="hero_tag text-gray_gradient">Building Product & Brands </p>
+      </div>
+      <div className="w-full h-full absolute inset-0">
+        <Canvas className="w-full h-full">
+          <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+          <HackerRoom />
+        </Canvas>
+      </div>
+    </section>
   )
 }
 
